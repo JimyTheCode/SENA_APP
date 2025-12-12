3d8d46164a08f80cf9e3c413d1fac77db367d793
Jimy Sebastian Angarita Triana <jimy_sangarita@soy.sena.edu.co>
2025-12-12 07:39:07 -0500

    Creacion de CRUD Cursos y Modificacion de archivos

--- SENA_APP/cursos/views.py
+++ SENA_APP/cursos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.template import loader
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Curso
+from .forms import CursoForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -28,3 +32,74 @@ def detalle_curso(request, curso_id):
     }
     
     return HttpResponse(template.render(context, request))
+
+
+# VISTAS BASADAS EN CLASES - CRUD CURSO
+
+# CREATE - CURSO
+class CursoCreateView(generic.CreateView):
+    """Vista para crear un nuevo curso"""
+    model = Curso
+    form_class = CursoForm
+    template_name = 'agregar_curso.html'
+    success_url = reverse_lazy('cursos:lista_cursos')
+    
+    def form_valid(self, form):
+        """Mostrar mensaje de éxito al crear el curso"""
+        messages.success(
+            self.request,
+            f'El curso {form.instance.nombre} ha sido registrado exitosamente.'
+        )
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        """Mostrar mensaje de error si el formulario es inválido"""
+        messages.error(
+            self.request,
+            'Por favor, corrija los errores en el formulario.'
+        )
+        return super().form_invalid(form)
+
+
+# UPDATE - CURSO
+class CursoUpdateView(generic.UpdateView):
+    """Vista para actualizar un curso existente"""
+    model = Curso
+    form_class = CursoForm
+    template_name = 'editar_curso.html'
+    success_url = reverse_lazy('cursos:lista_cursos')
+    pk_url_kwarg = 'curso_id'
+    
+    def form_valid(self, form):
+        """Mostrar mensaje de éxito al actualizar"""
+        messages.success(
+            self.request,
+            f'El curso {form.instance.nombre} ha sido actualizado exitosamente.'
+        )
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        """Mostrar mensaje de error si el formulario es inválido"""
+        messages.error(
+            self.request,
+            'Por favor, corrija los errores en el formulario.'
+        )
+        return super().form_invalid(form)
+
+
+# DELETE - CURSO
+class CursoDeleteView(generic.DeleteView):
+    """Vista para eliminar un curso"""
+    model = Curso
+    template_name = 'eliminar_curso.html'
+    success_url = reverse_lazy('cursos:lista_cursos')
+    pk_url_kwarg = 'curso_id'
+    
+    def delete(self, request, *args, **kwargs):
+        """Mostrar mensaje de éxito al eliminar"""
+        curso = self.get_object()
+        messages.success(
+            request,
+            f'El curso {curso.nombre} ha sido eliminado exitosamente.'
+        )
+        return super().delete(request, *args, **kwargs)
